4f318003ffb9e6f73ebd887c56ccea0750cadb02
leinad2018 <leinad2018@gmail.com>
2017-01-10 19:02:02 -0600

    fixed bug where multiple submissions interfeared

---
+++ ProgComp/asdf/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println(input.nextLine());
+		System.out.println(input.nextLine());
+	}
+}
