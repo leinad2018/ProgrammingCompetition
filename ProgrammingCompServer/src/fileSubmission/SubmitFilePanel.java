a2e2b6c7c9dfd64c5dc8cf28287f1e41faf3f0b4
Tim Vrakas <tim.vrakas@gmail.com>
2017-01-20 19:14:08 -0600

    File selector re-built, and now remembers past directory.

--- ProgrammingCompServer/src/fileSubmission/SubmitFilePanel.java
+++ ProgrammingCompServer/src/fileSubmission/SubmitFilePanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,49 +14,73 @@ import generalPage.GeneralWindow;
 import main.Main;
 
 public class SubmitFilePanel extends JPanel {
-	private static final long serialVersionUID=1L;
+	private static final long serialVersionUID = 1L;
 	private ComboBoxDemo comboBox;
 	private JButton chooseFileButton, submitButton;
-	private JLabel fileChosen;
-	private File chosenOne=null;
+	private JLabel filePathLabel;
+	private File chosenFile = null;
+	private File lastFileDir;
 
 	public SubmitFilePanel() {
-		final SubmitFilePanel me=this;
-		
-		Main.setToNotifyOnFileChoose(this);
-		comboBox=new ComboBoxDemo();
-		chooseFileButton=new JButton("Choose file...");
+		final SubmitFilePanel me = this;
+
+		// Main.setToNotifyOnFileChoose(this);
+		comboBox = new ComboBoxDemo();
+		filePathLabel = new JLabel("No file choosen.");
+
+		chooseFileButton = new JButton("Choose file...");
 		chooseFileButton.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
-				Main.tryToChooseFile();
+				File selected = tryToChooseFile();
+				if (selected != null && selected.exists() && selected.canRead()) {
+					submitButton.setEnabled(true);
+					chosenFile = selected;
+					setPathText(selected);
+				} else {
+					System.out.println("Could not Find/Read File");
+				}
 			}
 		});
-		fileChosen=new JLabel("No file choosen.");
-		submitButton=new JButton("Submit");
+
+		submitButton = new JButton("Submit");
 		submitButton.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				me.onSubmitClicked();
 			}
 		});
 		submitButton.setEnabled(false);
+
 		add(comboBox);
 		add(chooseFileButton);
-		add(fileChosen);
+		add(filePathLabel);
 		add(submitButton);
 	}
-	
-	public void onFileChoose(File choosen) {
-		chosenOne=choosen;
-		submitButton.setEnabled(true);
-		String path=chosenOne.getPath();
-		if (path.length()>25) {
-			path="..."+path.substring(path.length()-22);
+
+	public void setPathText(File file) {
+		String path = chosenFile.getPath();
+		if (path.length() > 25) {
+			path = "..." + path.substring(path.length() - 22);
 		}
-		fileChosen.setText(path);
+		filePathLabel.setText(path);
 	}
-	
+
 	public void onSubmitClicked() {
-		Main.submitFileToServer(chosenOne, Main.username, comboBox.getSelected());
+		Main.submitFileToServer(chosenFile, Main.username, comboBox.getSelected());
 		GeneralWindow.main.setContentType(ContentTypes.VIEW_SUBMISSION_HISTORY);
 	}
+
+	public File tryToChooseFile() {
+		JFileChooser chooser = new JFileChooser();
+		if (lastFileDir != null)
+			chooser.setCurrentDirectory(lastFileDir);
+		int returnVal = chooser.showOpenDialog(this);
+		lastFileDir = chooser.getCurrentDirectory();
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+			return chooser.getSelectedFile();
+		else
+			return null; // throw?
+	}
+
 }
--- ProgrammingCompServer/src/main/Main.java
+++ ProgrammingCompServer/src/main/Main.java
@@ -2,68 +2,44 @@ package main;
 
 import java.io.File;
 
-import fileSubmission.SubmissionWindow;
-import fileSubmission.SubmitFilePanel;
 import generalPage.GeneralWindow;
 import loginPage.LoginWindow;
 import networking.Client;
 
 public class Main {
 
-	public static volatile File fileChosen=null;
-	private static SubmitFilePanel toNotifyOnFileChoose=null;
 	public static String username;
-	
+
 	public static void main(String[] args) {
 		Client.clientInit();
 		tryToLogin();
 	}
 
-	
 	public static void wait(double seconds) {
 		try {
-			Thread.sleep((long)(seconds*1000));
+			Thread.sleep((long) (seconds * 1000));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void tryToLogin() {
-		LoginWindow loginWindow=new LoginWindow();
+		LoginWindow loginWindow = new LoginWindow();
 		while (!loginWindow.areCredentialsVerified()) {
 			wait(0.1);
 		}
-		username=loginWindow.getUsername();
+		username = loginWindow.getUsername();
 		openGeneralWindow();
 	}
-	
+
 	public static void openGeneralWindow() {
-		GeneralWindow mainWindow=new GeneralWindow();
+		GeneralWindow mainWindow = new GeneralWindow();
 	}
-	
-	public static File tryToChooseFile() {
-		SubmissionWindow selectionWindow=new SubmissionWindow();
-		//If it worked, return the file, otherwise, return null
-		if (selectionWindow.chooserWorked()) {			
-			fileChosen=selectionWindow.getFile();
-			if (toNotifyOnFileChoose!=null) {
-				toNotifyOnFileChoose.onFileChoose(fileChosen);
-			}
-		}
-		else {
-			fileChosen=null;
-		}
-		return fileChosen;
-	}
-	
-	public static void setToNotifyOnFileChoose(SubmitFilePanel newPanel) {
-		toNotifyOnFileChoose=newPanel;
-	}
-	
+
 	public static void submitFileToServer(File toSubmit, String username, int questionNumber) {
 		Client.submitFile(username, questionNumber, toSubmit.getPath());
 	}
-	
+
 	public static void submitRequestForClarification(String clarification, String username, int questionNumber) {
 		Client.submitRequestForClarification(username, questionNumber, clarification);
 	}
